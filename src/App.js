cc368d6ac153584ff717ee9eabe5cec35ca5b714
“AndreuMartinez” <andreu.martinez.garcia.1992@gmail.com>
2022-05-11 11:48:22 +0200

    feat: implementada estructura basica de rutas en app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Footer from './Components/Footer/Footer';
+import Header from './Components/Header/Header';
 
 function App() {
   return (
-    <div className="App">
 
+    <div className="App">
+      <BrowserRouter>
+
+        <Header />
+
+        <Routes>
+
+          {/* <Route path={'/'} element={<Home />} />
+          <Route path={'/profile'} element={<Profile />} />
+          <Route path={'/register'} element={<Register />} />
+          <Route path={'/login'} element={<Login />} /> */}
+
+        </Routes>
+
+        <Footer />
+        
+      </BrowserRouter>
     </div>
   );
 }
