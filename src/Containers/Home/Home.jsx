41427f22089fc53b8c55874a930cc370bd062f90
“AndreuMartinez” <andreu.martinez.garcia.1992@gmail.com>
2022-05-11 14:06:47 +0200

    feat: card de redireccion a login o registro finalizada

--- src/Containers/Home/Home.jsx
+++ src/Containers/Home/Home.jsx
@@ -1,29 +1,44 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import {useNavigate} from 'react-router-dom'
-import {connect} from 'react-redux'
- 
+import { useNavigate } from 'react-router-dom'
+import { connect } from 'react-redux'
+
 import './Home.css'
- 
+
 const Home = (props) => {
- 
+
     let navigate = useNavigate();
- 
-    useEffect(()=>{
-    //UseEffect equivalente a componentDidMount (montado)
- 
-    },[])
- 
-    useEffect(()=>{
-    //UseEffect equivalente a componentDidUpdate (actualizado)
- 
-    },)
- 
- 
+
+    useEffect(() => {
+        //UseEffect equivalente a componentDidMount (montado)
+
+    }, [])
+
+    useEffect(() => {
+        //UseEffect equivalente a componentDidUpdate (actualizado)
+
+    })
+
+
     return (
-        <div className='designHome'>SOY HOME</div>
+        <div className='designHome'>
+            <div className="cardHome">
+                <h1 className='h1Home'>Wellcome to Simula CFD</h1>
+                <div class="card w-20 cardBody">
+                    <div class="card-body ">
+                        <h5 class="card-title">User access</h5>
+                        <p class="card-text">Identify yourself on the platform to continue : </p>
+                        <div className="optionHome">
+                            <a href="#" class="btn btn-primary">Sign up</a>
+                            <div className="spaceHomeAcces">or</div>
+                            <a href="#" class="btn btn-primary">Log in</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
-export default connect((state) => ({ 
+export default connect((state) => ({
     //variables de rdx a crear
 }))(Home);
