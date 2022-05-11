b1af504a8baea5027582269ebeeacdd6096eb165
“AndreuMartinez” <andreu.martinez.garcia.1992@gmail.com>
2022-05-11 11:45:35 +0200

    feat: configurado index.js para el funcionamiento de redux

--- src/index.js
+++ src/index.js
@@ -7,14 +7,14 @@ import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import store from './Redux/store';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+        <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
+); 
+
 reportWebVitals();
