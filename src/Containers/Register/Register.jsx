9b650acff76d5b6bf38c7bee0b660ecc570659ab
“AndreuMartinez” <andreu.martinez.garcia.1992@gmail.com>
2022-05-12 15:18:45 +0200

    fix: correjido error de variables sin usar y que no han sido limpiadas en el container de Registro

--- src/Containers/Register/Register.jsx
+++ src/Containers/Register/Register.jsx
@@ -70,7 +70,6 @@ const Register = (props) => {
 
     //RENDER
 
-    const phoneRegExp = /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/;
     //const dispatch = useDispatch();
     return (
 
