b46d3828fd696b046992aebb27ddaa8ea67b6f5d
“AndreuMartinez” <andreu.martinez.garcia.1992@gmail.com>
2022-05-12 16:57:06 +0200

    feat: estructura basica componente deskboard.jsx creada

---
+++ src/Containers/Deskboard/Deskboard.jsx
@@ -0,0 +1,29 @@
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import {useNavigate} from 'react-router-dom'
+import {connect} from 'react-redux'
+ 
+import './Deskboard.css'
+ 
+const Deskboard = (props) => {
+ 
+    let navigate = useNavigate();
+ 
+    useEffect(()=>{
+    //UseEffect equivalente a componentDidMount (montado)
+ 
+    },[])
+ 
+    useEffect(()=>{
+    //UseEffect equivalente a componentDidUpdate (actualizado)
+ 
+    },)
+ 
+ 
+    return (
+        <div className='designDeskboard'>DESK BOARD</div>
+    )
+}
+export default connect((state) => ({ 
+    //variables de rdx a crear
+}))(Deskboard);
