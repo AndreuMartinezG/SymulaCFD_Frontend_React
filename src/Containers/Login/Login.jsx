481483674019f7a9885acb35de1e4ef11af954e3
“AndreuMartinez” <andreu.martinez.garcia.1992@gmail.com>
2022-05-12 15:37:00 +0200

    feat:diseño de login finalizado y adaptado a movil

--- src/Containers/Login/Login.jsx
+++ src/Containers/Login/Login.jsx
@@ -5,7 +5,7 @@ import axios from 'axios'
 import { Formik } from 'formik';
 import * as Yup from 'yup';
 import { Form, Input, Button, Checkbox, Typography, } from 'antd';
-import {LockOutlined, UserOutlined} from '@ant-design/icons'
+import { LockOutlined, UserOutlined } from '@ant-design/icons'
 //REDUX
 
 import { connect } from 'react-redux'
@@ -20,7 +20,7 @@ const Login = (props) => {
     let navigate = useNavigate();
 
     //1-Hooks (equivalen al estado en los componentes de clase)
-    
+
     // const [datosUsuario, setDatosUsuario] = useState({ email: "", password: "" });
 
 
@@ -60,28 +60,30 @@ const Login = (props) => {
             //Me invento las credenciales
 
             let body = dataToSubmit
-            
-            let resultado = await axios.post("https://movie-db-geekshubs.herokuapp.com/usuarios/login", body);
-            
+
+            console.log(body)
+
+            // let resultado = await axios.post("https://movie-db-geekshubs.herokuapp.com/usuarios/login", body);
+
 
             //Cambiamos el valor del hook credenciales, por lo tanto se recargará el componente
-            if (resultado.data === "Usuario o contraseña inválido") {
-                alert("Usuario o contraseña inválido")
-                setTimeout(() => {
-                    window.location.reload()
-                },1000)
-                navigate("/login")
-            } else {
+            // if (resultado.data === "Usuario o contraseña inválido") {
+            //     alert("Usuario o contraseña inválido")
+            //     setTimeout(() => {
+            //         window.location.reload()
+            //     },1000)
+            //     navigate("/login")
+            // } else {
 
-                //Guardaríamos los datos en redux...
+            //     //Guardaríamos los datos en redux...
 
-                props.dispatch({ type: LOGIN, payload: resultado.data });
+            //     props.dispatch({ type: LOGIN, payload: resultado.data });
 
 
-                setTimeout(() => {
-                    navigate("/");
-                }, 1500);
-            }
+            //     setTimeout(() => {
+            //         navigate("/");
+            //     }, 1500);
+            // }
 
 
         } catch (error) {
@@ -107,7 +109,7 @@ const Login = (props) => {
                     .required('Password is required'),
             })}
             onSubmit={(values, { setSubmitting }) => {
-                
+
                 setTimeout(() => {
                     let dataToSubmit = {
                         email: values.email,
@@ -134,65 +136,67 @@ const Login = (props) => {
                 } = props;
                 return (
                     <div className="app bg-color">
-                        <hr />
-                        <Title level={1}>Log In</Title>
                         
-                        <form onSubmit={handleSubmit} style={{ width: '350px' }}>
+                        <Title level={1}>Log In</Title>
+                        <hr />
+                        <div className="loginForm">
+                            <form onSubmit={handleSubmit} style={{ width: '350px' }}>
 
-                            <Form.Item required>
-                                <UserOutlined />
-                                <Input
-                                    id="email"
-                                    placeholder="Enter your email"
-                                    type="email"
-                                    value={values.email}
-                                    onChange={handleChange}
-                                    onBlur={handleBlur}
-                                    className={
-                                        errors.email && touched.email ? 'text-input error' : 'text-input'
-                                    }
-                                />
-                                {errors.email && touched.email && (
-                                    <div className="input-feedback">{errors.email}</div>
+                                <Form.Item required>
+                                    <UserOutlined />
+                                    <Input
+                                        id="email"
+                                        placeholder="Enter your email"
+                                        type="email"
+                                        value={values.email}
+                                        onChange={handleChange}
+                                        onBlur={handleBlur}
+                                        className={
+                                            errors.email && touched.email ? 'text-input error' : 'text-input'
+                                        }
+                                    />
+                                    {errors.email && touched.email && (
+                                        <div className="input-feedback">{errors.email}</div>
+                                    )}
+                                </Form.Item>
+
+                                <Form.Item required>
+                                    <LockOutlined />
+                                    <Input
+                                        id="password"
+
+                                        placeholder="Enter your password"
+                                        type="password"
+                                        value={values.password}
+                                        onChange={handleChange}
+                                        onBlur={handleBlur}
+                                        className={
+                                            errors.password && touched.password ? 'text-input error' : 'text-input'
+                                        }
+                                    />
+                                    {errors.password && touched.password && (
+                                        <div className="input-feedback">{errors.password}</div>
+                                    )}
+                                </Form.Item>
+
+                                {formErrorMessage && (
+                                    <label ><p style={{ color: '#ff0000bf', fontSize: '0.7rem', border: '1px solid', padding: '1rem', borderRadius: '10px' }}>{formErrorMessage}</p></label>
                                 )}
-                            </Form.Item>
 
-                            <Form.Item required>
-                                <LockOutlined />
-                                <Input
-                                    id="password"
-                                    
-                                    placeholder="Enter your password"
-                                    type="password"
-                                    value={values.password}
-                                    onChange={handleChange}
-                                    onBlur={handleBlur}
-                                    className={
-                                        errors.password && touched.password ? 'text-input error' : 'text-input'
-                                    }
-                                />
-                                {errors.password && touched.password && (
-                                    <div className="input-feedback">{errors.password}</div>
-                                )}
-                            </Form.Item>
-
-                            {formErrorMessage && (
-                                <label ><p style={{ color: '#ff0000bf', fontSize: '0.7rem', border: '1px solid', padding: '1rem', borderRadius: '10px' }}>{formErrorMessage}</p></label>
-                            )}
-
-                            <Form.Item>
-                                <Checkbox className="checkIzq" id="rememberMe" onChange={handleRememberMe} checked={rememberMe} >Remember me</Checkbox>
-                                <a className="login-form-forgot" href="/reset_user" style={{ float: 'right' }}>
-                                    forgot password
-                                </a>
-                                <div>
-                                    <Button type="primary" htmlType="submit" className="login-form-button" style={{ minWidth: '100%' }} disabled={isSubmitting} onSubmit={handleSubmit}>
-                                        Log in
-                                    </Button>
-                                </div>
-                                Or <a href="/register">register now!</a>
-                            </Form.Item>
-                        </form>
+                                <Form.Item>
+                                    <Checkbox className="checkIzq" id="rememberMe" onChange={handleRememberMe} checked={rememberMe} >Remember me</Checkbox>
+                                    <a className="login-form-forgot" href="/reset_user" style={{ float: 'right' }}>
+                                        forgot password
+                                    </a>
+                                    <div>
+                                        <Button type="primary" htmlType="submit" className="login-form-button" style={{ minWidth: '100%' }} disabled={isSubmitting} onSubmit={handleSubmit}>
+                                            Log in
+                                        </Button>
+                                    </div>
+                                    Or <a href="/register">register now!</a>
+                                </Form.Item>
+                            </form>
+                        </div>
                     </div>
                 );
             }}
