866b55df2928099ba1fa501cacec62dcd237ad65
“AndreuMartinez” <andreu.martinez.garcia.1992@gmail.com>
2022-05-14 12:53:11 +0200

    feat:Primer modal de creacion de proyecto finalizado

--- src/Containers/Deskboard/Deskboard.jsx
+++ src/Containers/Deskboard/Deskboard.jsx
@@ -39,14 +39,14 @@ const Deskboard = (props) => {
         })
 
         //get data from inputGroupSelect03
-        let select = document.getElementById('inputGroupSelect03');
-        console.log(select.value);
+        let categorySelected = document.getElementById('category');
+        console.log(categorySelected.value);
     }
 
     return (
         <div className='designDeskboard'><h1>DESK BOARD</h1>
 
-            <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+            {/* <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
                 Launch demo modal
             </button>
 
@@ -58,10 +58,70 @@ const Deskboard = (props) => {
                             <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
                         <div className="modal-body">
-                            <input type="file" id="file" name="myImage" accept=".stl" onChange={(e) => convertBase64(e.target.files)} />
+
+
+                            <div className="input-group mb-3">
+                                <input type="text" className="form-control" aria-label="Title" aria-describedby="Title" />
+                            </div>
+
+
+                            <label htmlFor="basic-input" className="form-label">Category</label>
+                            <div className="input-group mb-3">
+                                <select className="form-select" id="category" aria-label="Example select with button addon">
+                                    <option value="Aeroespacial">Aeroespacial</option>
+                                    <option value="Automoción">Automoción</option>
+                                    <option value="Validación">Validación</option>
+                                    <option value="Hobby">Hobby</option>
+                                    <option value="Profesional">Profesional</option>
+                                    <option value="Estudiante">Estudiante</option>
+                                    <option value="Formación">Formación</option>
+                                    <option value="Otro">Otro</option>
+                                </select>
+                            </div>
+                            <div className="mb-3">
+                                <label htmlFor="formFile" className="form-label">Select a file .stl</label>
+                                <input className="form-control" type="file" id="file" name="myImage" accept=".stl" onChange={(e) => convertBase64(e.target.files)} />
+                            </div>
+                        </div>
+                        *Each geometry to study will be considered as an independent project
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <button type="button" className="btn btn-primary" onClick={() => save()}>Save changes</button>
+                        </div>
+                    </div>
+                </div>
+            </div> */}
+
+            {/* Segundo modal */}
+
+            <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModalToggle">
+                New Project
+            </button>
+            <div className="modal fade" id="exampleModalToggle" aria-hidden="true" aria-labelledby="exampleModalToggleLabel" tabindex="-1">
+                <div className="modal-dialog modal-dialog-centered">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title" id="exampleModalToggleLabel">Project Data</h5>
+                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div className="modal-body">
+
+                            {/* TITULO */}
+                            <label htmlFor="basic-input" className="form-label">Title</label>
+                            <div className="input-group mb-3">
+                                <input type="text" className="form-control" aria-label="Title" aria-describedby="Title" />
+                            </div>
+
+                            {/* DESCRIPCION */}
+                            <label htmlFor="basic-input" className="form-label">Description</label>
+                            <div class="input-group">
+                                <textarea class="form-control" aria-label="With textarea"></textarea>
+                            </div>
+                            <br />
+                            {/* CATEGORIA */}
+                            <label htmlFor="basic-input" className="form-label">Category</label>
                             <div className="input-group mb-3">
                                 <select className="form-select" id="inputGroupSelect03" aria-label="Example select with button addon">
-                                    <option value="0">Category</option>
                                     <option value="Aeroespacial">Aeroespacial</option>
                                     <option value="Automoción">Automoción</option>
                                     <option value="Validación">Validación</option>
@@ -74,12 +134,31 @@ const Deskboard = (props) => {
                             </div>
                         </div>
                         <div className="modal-footer">
-                            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                            <button type="button" className="btn btn-primary" onClick={() => save()}>Save changes</button>
+                            <button className="btn btn-primary" data-bs-target="#exampleModalToggle2" data-bs-toggle="modal">Open second modal</button>
                         </div>
                     </div>
                 </div>
             </div>
+            <div className="modal fade" id="exampleModalToggle2" aria-hidden="true" aria-labelledby="exampleModalToggleLabel2" tabindex="-1">
+                <div className="modal-dialog modal-dialog-centered">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title" id="exampleModalToggleLabel2">Load 3D model</h5>
+                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div className="modal-body">
+                            <div className="mb-3">
+                                <label htmlFor="formFile" className="form-label">Select a file .stl</label>
+                                <input className="form-control" type="file" id="file" name="myImage" accept=".stl" onChange={(e) => convertBase64(e.target.files)} />
+                            </div>
+                        </div>
+                        <div className="modal-footer">
+                            <button className="btn btn-primary" data-bs-target="#exampleModalToggle" data-bs-toggle="modal">Back to first</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <a className="btn btn-primary" data-bs-toggle="modal" href="#exampleModalToggle" role="button">New Proyect</a>
             <div className="modal-dialog modal-xl">...</div>
         </div>
     )
