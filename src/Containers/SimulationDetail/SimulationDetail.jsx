7551db1bd7883924c344ef21fa268a45a3242196
“AndreuMartinez” <andreu.martinez.garcia.1992@gmail.com>
2022-05-15 17:48:44 +0200

    feat: configurado botones dentro del Iframe para acudir a la pasarela de pago

--- src/Containers/SimulationDetail/SimulationDetail.jsx
+++ src/Containers/SimulationDetail/SimulationDetail.jsx
@@ -1,33 +1,41 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import {useNavigate} from 'react-router-dom'
-import {connect} from 'react-redux'
- 
+import { useNavigate } from 'react-router-dom'
+import { connect } from 'react-redux'
+
 import './SimulationDetail.css'
- 
+
 const SimulationDetail = (props) => {
- 
+
     let navigate = useNavigate();
- 
-    useEffect(()=>{
-    //UseEffect equivalente a componentDidMount (montado)
+
+    useEffect(() => {
+        //UseEffect equivalente a componentDidMount (montado)
         console.log(props.search, 'props')
-    },[])
- 
-    useEffect(()=>{
-    //UseEffect equivalente a componentDidUpdate (actualizado)
- 
-    },)
+    }, [])
+
+    useEffect(() => {
+        //UseEffect equivalente a componentDidUpdate (actualizado)
+
+    })
     const urlIndex = props.search.index_Route_3D;
- 
+
     return (
         <div className='designSimulationDetail'>
-            <div className="pasarelaPago"></div>
-            <iframe className="pintar3DIndex" src={urlIndex}></iframe>
+
+            <div className="index3dContent">
+                <div className="pasarelaPago">
+                    <a class="btn btn-primary margenPago" href="https://buy.stripe.com/test_28o9Epcfg66e7sc3cd" role="button">Conceptual Simulation</a>
+                    <a class="btn btn-primary margenPago" href="https://buy.stripe.com/test_5kA17T7Z01PYh2M9AC" role="button">Balanced Simulation</a>
+                    <a class="btn btn-primary margenPago" href="https://buy.stripe.com/test_3cs9Ep6UWdyG9AkaEH" role="button">Accurate simulation</a>
+                </div>
+                <iframe className="pintar3DIndex" src={urlIndex}></iframe>
+            </div>
+
         </div>
     )
 }
-export default connect((state) => ({ 
+export default connect((state) => ({
     credentials: state.credentials,
     search: state.search.project
 }))(SimulationDetail);
