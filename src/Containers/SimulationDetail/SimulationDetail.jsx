40328475ea32f04bff4e96f96110b5168f86c9fb
“AndreuMartinez” <andreu.martinez.garcia.1992@gmail.com>
2022-05-15 17:59:02 +0200

    feat: finalizado diseño basico de SimulationDetail

--- src/Containers/SimulationDetail/SimulationDetail.jsx
+++ src/Containers/SimulationDetail/SimulationDetail.jsx
@@ -25,9 +25,9 @@ const SimulationDetail = (props) => {
 
             <div className="index3dContent">
                 <div className="pasarelaPago">
-                    <a class="btn btn-primary margenPago" href="https://buy.stripe.com/test_28o9Epcfg66e7sc3cd" role="button">Conceptual Simulation</a>
-                    <a class="btn btn-primary margenPago" href="https://buy.stripe.com/test_5kA17T7Z01PYh2M9AC" role="button">Balanced Simulation</a>
-                    <a class="btn btn-primary margenPago" href="https://buy.stripe.com/test_3cs9Ep6UWdyG9AkaEH" role="button">Accurate simulation</a>
+                    <a className="btn btn-primary margenPago" href="https://buy.stripe.com/test_28o9Epcfg66e7sc3cd" role="button">Conceptual Simulation</a>
+                    <a className="btn btn-primary margenPago" href="https://buy.stripe.com/test_5kA17T7Z01PYh2M9AC" role="button">Balanced Simulation</a>
+                    <a className="btn btn-primary margenPago" href="https://buy.stripe.com/test_3cs9Ep6UWdyG9AkaEH" role="button">Accurate simulation</a>
                 </div>
                 <iframe className="pintar3DIndex" src={urlIndex}></iframe>
             </div>
