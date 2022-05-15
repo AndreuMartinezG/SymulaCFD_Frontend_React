cb0bdd14bf1695d0581ca233568d169a73c1f2cd
“AndreuMartinez” <andreu.martinez.garcia.1992@gmail.com>
2022-05-15 15:50:24 +0200

    feat: comenzando diseño de ProjectDetail

--- src/Containers/ProjectDetail/ProjectDetail.jsx
+++ src/Containers/ProjectDetail/ProjectDetail.jsx
@@ -1,29 +1,37 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import {useNavigate} from 'react-router-dom'
-import {connect} from 'react-redux'
- 
+import { useNavigate } from 'react-router-dom'
+import { connect } from 'react-redux'
+
 import './ProjectDetail.css'
- 
+
 const ProjectDetail = (props) => {
- 
+
     let navigate = useNavigate();
- 
-    useEffect(()=>{
-    //UseEffect equivalente a componentDidMount (montado)
- 
-    },[])
- 
-    useEffect(()=>{
-    //UseEffect equivalente a componentDidUpdate (actualizado)
- 
-    },)
- 
- 
+
+    useEffect(() => {
+        //UseEffect equivalente a componentDidMount (montado)
+        console.log(props.search.default_Route_3D, 'props')
+    }, [])
+
+    useEffect(() => {
+        //UseEffect equivalente a componentDidUpdate (actualizado)
+    })
+
+    const urlDefault = props.search.default_Route_3D;
+
     return (
-        <div className='designProjectDetail'></div>
+        <div className='designProjectDetail'>
+            <div className="iframe3D">
+                <iframe src={urlDefault} width="100%" height="100%"></iframe>
+            </div>
+            <div className="datosProject">
+                sdf
+            </div>
+        </div>
     )
 }
-export default connect((state) => ({ 
-    //variables de rdx a crear
+export default connect((state) => ({
+    credentials: state.credentials,
+    search: state.search.project
 }))(ProjectDetail);
